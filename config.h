284ab165140c2a117b2ed8ca268547d829c7ae32
Graphity <rohood3@gmail.com>
2020-07-25 09:06:57 +0400

    Add keybind for pwr script

--- config.h
+++ config.h
@@ -128,7 +128,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_l,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_r,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      spawn,          SHCMD("pwr") },
 };
 
 /* button definitions */
