5d69b3c88fc793cdddaaa3edf9f6093d3b984a74
Graphity <rohood3@gmail.com>
2022-04-17 00:31:34 +0400

    Add 'swallow' to Alacritty

--- config.h
+++ config.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 	{ "Spotify",   NULL,     NULL,           1 << 4,    0,          0,          -1,        -1 },
 	{ "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "kitty",     NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ "alacritty", NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "Alacritty", NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,        NULL,     "Event Tester", 0,         1,          0,           1,        -1 },
 };
 
